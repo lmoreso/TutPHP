b12ce39121bf80ed56df7454d2972ffc8a1db66c
Lluis Moreso Bosch <lmoreso@yahoo.com>
2018-02-27 16:04:15 +0100

    Renombrada carpeta lib a .inc

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-    include('../lib/TutPHP/funciones.php');
+    include('../.inc/TutPHP/funciones.php');
 ?>
 <html>
     <?php
